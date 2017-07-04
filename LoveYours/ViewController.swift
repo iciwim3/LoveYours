3e866943b1d371a147e1e162adc2b1bf6ba9380b
SKE <appybuildmore@gmail.com>
2017-07-04 08:41:07 -0500

    1. Removed label from Storyboard. 2. Added a Tableview to SB. 3. Added new print statement to VC.

--- LoveYours/ViewController.swift
+++ LoveYours/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Heart beating fast, let a Player know that HE ALIVE!!!")
+        
+        print("As long as the people in that place love you dearly!")
     }
 
     override func didReceiveMemoryWarning() {
